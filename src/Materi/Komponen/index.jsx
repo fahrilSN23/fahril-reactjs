dfb5448a4c93ada36da52b3e5396e8737f57b720
fahrilsn23 <fahrilsariannur@gmail.com>
2023-02-27 13:48:59 +0900

    1. Tugas Membuat Komponen

---
+++ src/Materi/Komponen/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ClassComponent from "./pembahasan/ClassComponent";
+import FunctionalComponent from "./pembahasan/FunctionalComponent";
+
+export default class Komponen extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                <ClassComponent nama="Fahril Sarian Nur" />
+                <FunctionalComponent />
+            </div>
+        )
+
+    }
+
+}
